84394ba7ee5dc83f175d079c0dca0389868c5b77
Chris <chriswfoster@gmail.com>
2018-02-12 09:31:30 -0600

    Functioning password creation

--- src/components/TestLogin.js
+++ src/components/TestLogin.js
@@ -17,18 +17,24 @@ class TestLogin extends Component{
         this.setState({usernameText: val})
     }
     sendItBro(username, password){
-
+        axios.post('/api/sendlogin',{
+            username: username,
+            password: password
+        })
     }
+
 
     render(){
         return(
             <div >
+                <h1> Create a user account to test password encryption:</h1>
+                <h3> Let me know what you think, this is just an idea</h3>
                 <p> Username: </p>
                 <input placeholder="username" onChange={(e) => this.usernameTypeHandler(e.target.value)}/>
                 <p> Password: </p>
                 <input placeholder="password" onChange={(e) => this.passwordTypeHandler(e.target.value)}/>
                 <p style={{borderStyle: "solid", width: "10%"}} onClick={() => this.sendItBro(this.state.usernameText, this.state.passwordText)}>Submit button </p>
-
+                <h3> Now check the DB, you should see the hashed password </h3>
                 </div>
         )
     }
