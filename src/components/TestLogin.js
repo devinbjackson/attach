ae8b695dd41d92743af906bbd4c9e9b0525a0ee1
Chris <chriswfoster@gmail.com>
2018-02-12 09:11:48 -0600

    Basic login build

--- src/components/TestLogin.js
+++ src/components/TestLogin.js
@@ -1,21 +1,33 @@
-import React, {Componenet} from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
 
-class TestLogin extends Componenet{
+class TestLogin extends Component{
     constructor(){
         super()
         this.state = {
-
+            passwordText: "",
+            usernameText: ""
         }
+    }
+
+    passwordTypeHandler(val){
+        this.setState({passwordText: val})
+    }
+    usernameTypeHandler(val){
+        this.setState({usernameText: val})
+    }
+    sendItBro(username, password){
+
     }
 
     render(){
         return(
-            <div>
+            <div >
                 <p> Username: </p>
-                <input placeholder="username"/>
+                <input placeholder="username" onChange={(e) => this.usernameTypeHandler(e.target.value)}/>
                 <p> Password: </p>
-                <input placeholder="password" />
+                <input placeholder="password" onChange={(e) => this.passwordTypeHandler(e.target.value)}/>
+                <p style={{borderStyle: "solid", width: "10%"}} onClick={() => this.sendItBro(this.state.usernameText, this.state.passwordText)}>Submit button </p>
 
                 </div>
         )
